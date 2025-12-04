f273f8bda52ff6fe4a825b26e08ae2127fc9691a
Jack Bayliss <jjbayliss@icloud.com>
2025-12-04 16:21:39 +0000

    Update SupervisorOptionsTest.php

--- tests/Feature/SupervisorOptionsTest.php
+++ tests/Feature/SupervisorOptionsTest.php
@@ -16,7 +16,7 @@ class SupervisorOptionsTest extends IntegrationTest
     public function test_dynamic_property_warning_from_worker_pausable()
     {
         // Set the static pausable property on the worker instance
-        $workerClass = get_class($this->worker);
+        $workerClass = get_class($this->worker());
         $workerClass::$pausable = true;
 
         $this->assertTrue($workerClass::$pausable);
