931bc29dfb54efa970671349de1a32dd24980ca2
Jack Bayliss <jjbayliss@icloud.com>
2025-12-04 17:01:15 +0000

    Update SupervisorOptionsTest.php

--- tests/Feature/SupervisorOptionsTest.php
+++ tests/Feature/SupervisorOptionsTest.php
@@ -18,7 +18,7 @@ class SupervisorOptionsTest extends IntegrationTest
         if (version_compare($this->app->version(), '12.41.0', '<')) {
             $this->markTestSkipped('This test is not valid for Laravel < 12.41.0.');
         }
-
+        echo $this->app->version();
         $workerClass = get_class($this->worker());
         if(property_exists($workerClass, '$pausable')) {
             $workerClass::$pausable = false;
